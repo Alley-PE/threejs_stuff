964018eeb9ec796a7e98cb3ca3ff3bdb0fd70c0b
Alley-PE <alley.pe@protonmail.com>
2024-01-15 16:33:15 -0600

    Changed the order of scripts (SUPER IMPORTANT), removed the line at the top of mahCode which imported from the library

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let height = threejsCanvas.offsetHeight
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(70, width/height, 1, 1000)
+
 camera.position.set(10,10,10)
 camera.lookAt(0,0,0)
 
@@ -17,14 +18,20 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 threejsCanvas.appendChild(renderer.domElement)
 
 const geometry = new THREE.BoxGeometry(5,5,5)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ffff, wireframe: true })
+const material = new THREE.MeshBasicMaterial({ color: 0x00ffff})
 const box = new THREE.Mesh(geometry, material)
 
 scene.add(box)
 
 update ()
 
+
+
 function update () {
+
+    box.rotation.z =+ 0.005
+    box.rotation.y =+ 0.01
     renderer.render(scene,camera)
     window.requestAnimationFrame(update)
 }
+
--- mahCode.js
+++ mahCode.js
@@ -1,21 +1,24 @@
-import './three_style.css'
-
-import * as THREE from 'three';
+const threejsCanvas = document.querySelector('#threejs-canvas')
+let width = threejsCanvas.offsetWidth
+let height = threejsCanvas.offsetHeight
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-
+camera.position.set(10,10,10)
+camera.lookAt(0,0,0)
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#threejs-canvas'),
-});
+  antialias: true,
+  alpha: true,
+})
 
 renderer.setPixelRatio( Math.min(window.devicePixelRatio) );
 renderer.setSize( window.innerWidth, window.innerHeight );
+threejsCanvas.appendChild(renderer.domElement)
 
-camera.position.setZ(30);
+
 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial( { color: 0xFF6347, wireframe: true } ); 
