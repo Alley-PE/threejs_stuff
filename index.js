245f3dda16ff988400b3fda8ca933a56aa2454ef
Alley-PE <alley.pe@protonmail.com>
2024-01-15 15:39:07 -0600

    tried following a tutorial with the ones named index but the didn't do shit

---
+++ index.js
@@ -0,0 +1,30 @@
+const threejsCanvas = document.querySelector('#threejs-canvas')
+let width = threejsCanvas.offsetWidth
+let height = threejsCanvas.offsetHeight
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(70, width/height, 1, 1000)
+camera.position.set(10,10,10)
+camera.lookAt(0,0,0)
+
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true,
+})
+
+renderer.setSize(width, height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+threejsCanvas.appendChild(renderer.domElement)
+
+const geometry = new THREE.BoxGeometry(5,5,5)
+const material = new THREE.MeshBasicMaterial({ color: 0x00ffff, wireframe: true })
+const box = new THREE.Mesh(geometry, material)
+
+scene.add(box)
+
+update ()
+
+function update () {
+    renderer.render(scene,camera)
+    window.requestAnimationFrame(update)
+}
