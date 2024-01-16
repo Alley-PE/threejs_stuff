1cc7ec90e19d96bf7d9a4ecd662153d227d309e3
Alley-PE <alley.pe@protonmail.com>
2024-01-16 15:32:35 -0600

    made the code match the working copy to test something

--- mahCode.js
+++ mahCode.js
@@ -2,23 +2,19 @@ const threejsCanvas = document.querySelector('#threejs-canvas')
 let width = threejsCanvas.offsetWidth
 let height = threejsCanvas.offsetHeight
 
-const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(70,width/height,1,1000)
 camera.position.set(10,10,10)
 camera.lookAt(0,0,0)
 
 const renderer = new THREE.WebGLRenderer({
-  antialias: true,
-  alpha: true,
+    antialias: true,
+    alpha:true,
 })
 
-renderer.setPixelRatio( Math.min(window.devicePixelRatio) 2);
-renderer.setSize( width, height );
+renderer.setSize(width, height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 threejsCanvas.appendChild(renderer.domElement)
-
-
 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial( { color: 0xFF6347, wireframe: true } ); 
@@ -38,18 +34,18 @@ function animate() {
 
  
 
-  requestAnimationFrame( animate );
-
- 
-  torus.rotation.y += 0.005;
+    requestAnimationFrame( animate );
   
-
-
-  renderer.render( scene, camera );
+   
+    torus.rotation.y += 0.005;
+    
   
-}
-
-animate()
+  
+    renderer.render( scene, camera );
+    
+  }
+  
+  animate()
 
 
 
